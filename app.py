0456aa6331be3e6e592621d515ed4217ae1663ad
Francesco Traversa <francesco.traversa@bain.com>
2025-01-24 09:57:09 +0100

    New webhook test

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from webhoook import app
 
+print("Hello, World!")
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
